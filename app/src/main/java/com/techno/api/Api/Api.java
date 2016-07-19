5ed066b430ef3985360f9a9166596f908b074a1a
arbaz_thetatechnolabs <arbaz.thetatehnolabs@gmail.com>
2016-07-19 19:07:32 +0530

    last commit fro API

---
+++ app/src/main/java/com/techno/api/Api/Api.java
@@ -0,0 +1,25 @@
+package com.techno.api.Api;
+
+/**
+ * Created by arbaz on 29/6/16.
+ */
+public class Api {
+
+    //Dump Condition
+    public static final int ConnectionTimeout = 30000; // = 30 seconds
+    public static final int ConnectionSoTimeout = 60000; // = 60 seconds
+
+
+    //Response Codes
+    public static final int ResponseOk = 200;
+    public static final int ResponsePageError = 400;
+    public static final int ResponseServerError = 500;
+
+    // App URL
+    public static final String MainUrl = "http://192.168.1.12/salon/webservices/web/index.php";
+
+
+    public static final String ActionLogin = "/login";
+    public static final String ActionRegistration = "/user/register";
+
+}
